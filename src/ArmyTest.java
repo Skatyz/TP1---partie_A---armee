49021b659e3ca3d8e3d01f552035f16ff92944e8
Félix_Bédard_2138995 <filou869@outlook.com>
2022-10-21 11:26:11 -0400

    k

--- src/ArmyTest.java
+++ src/ArmyTest.java
@@ -1,7 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-class ArmyTest {
-ovnsdibvosDIBVosdBVosdVsDv
-    bdsBsdBSbDS
-        BDS
+class ArmyTest { //ovnsdibvosDIBVosdBVosdVsDv
+    //bdsBsdBSbDS
+      //  BDS
 }
