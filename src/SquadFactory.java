08560c936dbbece48e33b7c08c59a23ae9a14fcb
Félix_Bédard_2138995 <filou869@outlook.com>
2022-10-30 16:39:52 -0400

    k

--- src/SquadFactory.java
+++ src/SquadFactory.java
@@ -1,12 +1,16 @@
 public class SquadFactory {
     public Squad makeSquad(int characteristicPoints){
 
-        if (makeSquad(characteristicPoints).isOffensive()){
+        if (makeSquad(characteristicPoints).isOffensive() && characteristicPoints < 60){
             return new Cavalry(characteristicPoints);
+        }
+        else if (makeSquad(characteristicPoints).isOffensive() && characteristicPoints > 60){
             return new Sniper(characteristicPoints);
         }
-        if (makeSquad(characteristicPoints).isDeffensive()){
+        if (makeSquad(characteristicPoints).isDeffensive() && characteristicPoints < 60){
             return new Infantry(characteristicPoints);
+        }
+        else if (makeSquad(characteristicPoints).isDeffensive() && characteristicPoints > 60){
             return new Sapper(characteristicPoints);
         }
         if (makeSquad(characteristicPoints).isHealing()){
